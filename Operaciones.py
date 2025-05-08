90df88642cd940b666ac9d1653c54a2e2f3c199e
CrisRicDev <cristianrdavid2005@gmail.com>
2025-05-08 18:40:42 -0500

    Primer commit

---
+++ Operaciones.py
@@ -0,0 +1,14 @@
+def Suma(a,b):
+    try:
+        return suma = a + b
+    except ValueError:
+        print("numero no valido")
+
+
+def Resta(a,b):
+    try:
+        return resta = a - b
+    except ValueError:
+        print("numero no valido")
+
+
